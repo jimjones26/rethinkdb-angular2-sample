9bab0dd63f4be3f84f2460f0c4b15f888369420f
jimjones26 <jimjones26@gmail.com>
2017-01-02 16:19:24 -0500

    node server setup

--- server/server.ts
+++ server/server.ts
@@ -1,20 +1,27 @@
 import * as express from 'express';
 import { Application } from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+
 import { initRestApi } from "./api/api";
 import { apiErrorHandler } from "./api/apiErrorHandler";
 
-const serverString: string = 'Server is now running on port 8090 ...';
-
-const bodyParser = require('body-parser');
+require('dotenv').load();
 
 const app: Application = express();
 
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+app.use(apiErrorHandler);
 
 initRestApi(app);
 
-app.use(apiErrorHandler);
-
+const serverString: string = 'Server is now running on port 8090 ...';
 app.listen(8090, () => {
   console.log(serverString);
 });
