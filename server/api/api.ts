6319f32aadea602d3fd6f77f6db87e299c4ce6c8
jimjones26 <jimjones26@gmail.com>
2017-01-02 14:14:54 -0500

    more infrastructure in place

---
+++ server/api/api.ts
@@ -0,0 +1,5 @@
+import { Application } from "express";
+
+export function initRestApi(app: Application) {
+  
+}
--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,20 @@
-const testing: string = 'hello world';
+import * as express from 'express';
+import { Application } from 'express';
+import { initRestApi } from "./api/api";
+import { apiErrorHandler } from "./api/apiErrorHandler";
+
+const serverString: string = 'Server is now running on port 8090 ...';
+
+const bodyParser = require('body-parser');
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+
+initRestApi(app);
+
+app.use(apiErrorHandler);
+
+app.listen(8090, () => {
+  console.log(serverString);
+});
