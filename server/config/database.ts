e72d2554fd911dfd7a96890224b9d64ccf4b0ea7
jimjones26 <jimjones26@gmail.com>
2017-01-04 06:52:09 -0500

    update some stuff

--- server/config/database.ts
+++ server/config/database.ts
@@ -5,5 +5,6 @@ dotenv.config();
 module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    db: process.env.DB_NAME
+    db: process.env.DB_NAME,
+    user: process.env.DB_USER
 };
